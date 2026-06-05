2a5e3c8b946c6bd59d4efed481b63ebf2a73a3d1
Debadyuti Das <debadyutidas40@gmail.com>
2026-06-06 00:25:01 +0530

    Create call_Guests.java

---
+++ call_Guests.java
@@ -0,0 +1,16 @@
+public class call_Guests {
+    public static int callguests(int n){
+        if(n<=1){
+            return 1;
+        }
+        //single
+        int ways1=callguests(n-1);
+        //pair
+        int ways2=(n-1)*callguests(n-2);
+        return ways1+ways2;
+    }
+    public static void main(String[] args) {
+        int n=4;
+        System.out.println(callguests(n));
+    }
+}
