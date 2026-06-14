d11ad5c583c552ec31786058c09c42bbe5de62ff
Debadyuti Das <debadyutidas40@gmail.com>
2026-06-14 20:13:13 +0530

    Refactor Shape class for runtime polymorphism

--- Shape.java
+++ Shape.java
@@ -1,11 +1,29 @@
-//single inheritence
-class Shape {
-   public void area() {
-       System.out.println("Displays Area of Shape");
-   }
+//run time polymorphism
+class Shape1{
+    public static void area(){
+        System.out.println("display area:");
+        return ;
+    }
 }
-class Triangle extends Shape {
-   public void area(int h, int b) {
-       System.out.println((1/2)*b*h);
-   }  
-}
+    class Triangle extends Shape1 {
+        public static void area(int L,int H){
+            System.out.println(0.5*L*H);
+        }    
+        
+    }
+    class Circle extends Shape1 {
+        public static void area(int r){
+            System.out.println(3.14*r*r);
+
+        }
+    }
+    class Shape {
+        public static void main(String[] args) {
+        Shape1 s1=new Shape1();
+        s1.area();
+        Triangle t1=new Triangle();
+        t1.area(3,2);
+        Circle c1=new Circle();
+        c1.area(3);
+    }
+     }
